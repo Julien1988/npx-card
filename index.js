659e76baaaaad6c3303392015eee48ae312be1ea
Julien <julienbroens@gmail.com>
2020-05-08 15:00:50 +0200

    ajout des différentes infirmations

--- index.js
+++ index.js
@@ -2,18 +2,50 @@
 
 const chalk = require("chalk");
 const boxen = require("boxen");
-const margin = 1;
+const margin = 0;
 
 const info = {
-  name: chalk.white(" - Julien Broens -"),
-  job: chalk.white("-- Developer Web --"),
-  line: chalk.white("-- Developer Web --"),
+  name: chalk.white("Julien Broens"),
+  job: chalk.white(" Developer Web "),
+  line: chalk.cyan("--"),
+  soloLine: chalk.cyan("-"),
+  borderLine: chalk.cyan(
+    " ----------------------------------------------------------- "
+  ),
+  space: " ",
+  email: chalk.bold.red("email:"),
+  emailAdressPart1: chalk.magenta("julienbroens"),
+  emailAdressPart2: chalk.bold.yellow("@"),
+  emailAdressPart3: chalk.magenta("gmail.com"),
+  cv: chalk.bold.red("CV:"),
+  cvAdress: chalk.magenta("https://julien1988.github.io/julien-broens-cv/"),
+  github: chalk.bold.red("Github:"),
+  githubAdress: chalk.magenta("https://github.com/Julien1988"),
+  linkedin: chalk.bold.red("Linkedin:"),
+  linkedinAdress: chalk.magenta(
+    "https://linkedin.com/in/julien-broens-24b465184"
+  ),
 };
 console.log(
-  boxen([`${info.name}`, `${info.job}`].join("\n"), {
-    padding: margin,
-    margin: margin,
-    borderStyle: "double",
-    borderColor: "cyan",
-  })
+  boxen(
+    [
+      ` `,
+      `                      ${info.soloLine} ${info.name} ${info.soloLine}`,
+      `                   ${info.line} ${info.job} ${info.line}`,
+      ` `,
+      `${info.borderLine}`,
+      ` `,
+      `  ${info.email} ${info.emailAdressPart1}${info.emailAdressPart2}${info.emailAdressPart3}`,
+      `  ${info.cv} ${info.cvAdress}`,
+      `  ${info.github} ${info.githubAdress}`,
+      `  ${info.linkedin} ${info.linkedinAdress}  `,
+      ` `,
+    ].join("\n"),
+    {
+      padding: margin,
+      margin: margin,
+      borderStyle: "double",
+      borderColor: "cyan",
+    }
+  )
 );
